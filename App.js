5887b7538834952588b26d3a657dba99bd3611d5
chaudharycoder <ankit2019ragini@gmail.com>
2024-04-08 13:39:46 +0530

    add

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
-// const heading=React.createElement
-// ("h1",
-// {id:"heading",class:"myroom",xyz:"ankit"},
-// "Hello world from React!");
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading=React.createElement
-("div",
-{id:"parent"},
-[React.createElement("div",{id:"child1"},
-[React.createElement("h1",{},"I am h1 toy"),React.createElement("h1",{},"I am also sunil")]),React.createElement("div",{id:"child2"},
-[React.createElement("h1",{},"I am h1 tag"),React.createElement("h1",{},"I am also h1 tag")])]);
-console.log(heading);
-    const root=ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+
+const Header=()=>{
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://banner2.cleanpng.com/20180702/vrt/kisspng-online-food-ordering-food-delivery-grubhub-5b3a1b75a36132.9626073015305347736692.jpg"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>about</li>
+                    <li>contact</li>
+                    <li>cart</li>
+                </ul>
+
+            </div>
+
+        </div>
+    );
+};
+
+const AppLayout=()=>{
+    return (
+        <div className="app">
+        <Header />
+        </div>
+    )
+}
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
+
